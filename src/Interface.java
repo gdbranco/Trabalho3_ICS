dc46113464029a57f9f19be46f343fe7cacba6a1
Shurd <Shurd@Shurd-PC>
2013-11-21 11:12:38 -0200

    Feito um switch para passar as notas do midi para frequencia e adicionado as funcoes do sheppard tone

--- src/Interface.java
+++ src/Interface.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.sound.midi.InvalidMidiDataException;
@@ -33,7 +34,10 @@ import sintese.*;
 public class Interface extends JFrame {
 	private int NOTE_ON = 144;
 	private int NOT_OFF = 128;
-	private Vector lista = null;
+	private ArrayList<String> lista;
+	private double A0 = 0.7;
+	private int T = 20;
+	private int F0 = 220;
 	private static final long serialVersionUID = 1L;
 
 	private File midi=null;
@@ -55,11 +59,400 @@ public class Interface extends JFrame {
 			}
 		});
 	}
-
+	public void getTone()
+	{
+		Curva curva = new Curva(T);
+		for(int t=0;t<20;t++)
+		{
+			curva.addPonto(t, fS(t));
+			System.out.println(fS(t));
+		}
+		Envoltoria env = new Envoltoria(curva);
+		env.setGanho(250000f);
+		Som som = new Som(env,10f,"Teste");
+		som.visualiza();
+	}
+	//Para t E [0,T[
+	private double fS(double t)
+	{
+		return fA(t)*Math.sin(fW(t)*t)+fA(t+T)*Math.sin(2*fW(t)*t);
+	}
+	private double fA(double t)
+	{
+		return A0*((1+Math.cos(Math.PI)*t/T)/4);
+	}
+	private double fW(double t)
+	{
+		return 2*Math.PI*F0*(1/Math.pow(2, t/T));
+	}
+	private void inicia_lista()
+	{
+		lista.add("0C");lista.add("0C#");lista.add("0D");lista.add("0D#");lista.add("0E");lista.add("0E#");lista.add("0F");lista.add("0F#");lista.add("0G");lista.add("0G#");lista.add("0A");lista.add("0A#");lista.add("0B");
+		lista.add("1C");lista.add("1C#");lista.add("1D");lista.add("1D#");lista.add("1E");lista.add("1E#");lista.add("1F");lista.add("1F#");lista.add("1G");lista.add("1G#");lista.add("1A");lista.add("1A#");lista.add("1B");
+		lista.add("2C");lista.add("2C#");lista.add("2D");lista.add("2D#");lista.add("2E");lista.add("2E#");lista.add("0F");lista.add("2F#");lista.add("2G");lista.add("2G#");lista.add("2A");lista.add("2A#");lista.add("2B");
+		lista.add("3C");lista.add("3C#");lista.add("3D");lista.add("3D#");lista.add("3E");lista.add("3E#");lista.add("0F");lista.add("3F#");lista.add("3G");lista.add("3G#");lista.add("3A");lista.add("3A#");lista.add("3B");
+		lista.add("4C");lista.add("4C#");lista.add("4D");lista.add("4D#");lista.add("4E");lista.add("4E#");lista.add("0F");lista.add("4F#");lista.add("4G");lista.add("4G#");lista.add("4A");lista.add("4A#");lista.add("4B");
+		lista.add("5C");lista.add("5C#");lista.add("5D");lista.add("5D#");lista.add("5E");lista.add("5E#");lista.add("0F");lista.add("5F#");lista.add("5G");lista.add("5G#");lista.add("5A");lista.add("5A#");lista.add("5B");
+		lista.add("6C");lista.add("6C#");lista.add("6D");lista.add("6D#");lista.add("6E");lista.add("6E#");lista.add("0F");lista.add("6F#");lista.add("6G");lista.add("6G#");lista.add("6A");lista.add("6A#");lista.add("6B");
+		lista.add("7C");lista.add("7C#");lista.add("7D");lista.add("7D#");lista.add("7E");lista.add("7E#");lista.add("0F");lista.add("7F#");lista.add("7G");lista.add("7G#");lista.add("7A");lista.add("7A#");lista.add("7B");
+		lista.add("8C");lista.add("8C#");lista.add("8D");lista.add("8D#");lista.add("8E");lista.add("8E#");lista.add("0F");lista.add("8F#");lista.add("8G");lista.add("8G#");lista.add("8A");lista.add("8A#");lista.add("8B");
+		lista.add("9C");lista.add("9C#");lista.add("9D");lista.add("9D#");lista.add("9E");lista.add("9E#");lista.add("9F");lista.add("9F#");lista.add("9G");lista.add("9G#");lista.add("9A");lista.add("9A#");lista.add("9B");
+		lista.add("10C");lista.add("10C#");lista.add("10D");lista.add("10D#");lista.add("10E");lista.add("10E#");lista.add("10F");lista.add("10F#");lista.add("10G");
+	}
+	private float numNote_freq(int numNote)
+	{
+		float freq=0;
+		switch(numNote)
+		{
+		case 0:
+			freq = (float) 8.18;
+			break;
+		case 1:
+			freq = (float) 8.66;
+			break;
+		case 2:
+			freq = (float) 9.18;
+			break;
+		case 3:
+			freq = (float) 9.73;
+			break;
+		case 4:
+			freq = (float) 10.30;
+			break;
+		case 5:
+			freq = (float) 10.92;
+			break;
+		case 6:
+			freq = (float) 11.56;
+			break;
+		case 7:
+			freq = (float) 12.25;
+			break;
+		case 8:
+			freq = (float) 12.98;
+			break;
+		case 9:
+			freq = (float) 13.75;
+			break;
+		case 10:
+			freq = (float) 14.57;
+			break;
+		case 11:
+			freq = (float) 15.44;
+			break;
+		case 12:
+			freq = (float) 16.35;
+			break;
+		case 13:
+			freq = (float) 17.32;
+			break;
+		case 14:
+			freq = (float) 18.35;
+			break;
+		case 15:
+			freq = (float) 19.45;
+			break;
+		case 16:
+			freq = (float) 20.60;
+			break;
+		case 17:
+			freq = (float) 21.83;
+			break;
+		case 18:
+			freq = (float) 23.12;
+			break;
+		case 19:
+			freq = (float) 24.50;
+			break;
+		case 20:
+			freq = (float) 25.95;
+			break;
+		case 21:
+			freq = (float) 27.50;
+			break;
+		case 22:
+			freq = (float) 29.14;
+			break;
+		case 23:
+			freq = (float) 30.87;
+			break;
+		case 24:
+			freq = (float) 32.70;
+			break;
+		case 25:
+			freq = (float) 34.65;
+			break;
+		case 26:
+			freq = (float) 36.71;
+			break;
+		case 27:
+			freq = (float) 38.89;
+			break;
+		case 28:
+			freq = (float) 41.20;
+			break;
+		case 29:
+			freq = (float) 43.65;
+			break;
+		case 30:
+			freq = (float) 46.25;
+			break;
+		case 31:
+			freq = (float) 49.00;
+			break;
+		case 32:
+			freq = (float) 51.91;
+			break;
+		case 33:
+			freq = (float) 55.00;
+			break;
+		case 34:
+			freq = (float) 58.27;
+			break;
+		case 35:
+			freq = (float) 61.74;
+			break;
+		case 36:
+			freq = (float) 65.41;
+			break;
+		case 37:
+			freq = (float) 69.30;
+			break;
+		case 38:
+			freq = (float) 73.42;
+			break;
+		case 39:
+			freq = (float) 77.78;
+			break;
+		case 40:
+			freq = (float) 82.41;
+			break;
+		case 41:
+			freq = (float) 87.31;
+			break;
+		case 42:
+			freq = (float) 92.50;
+			break;
+		case 43:
+			freq = (float) 98.00;
+			break;
+		case 44:
+			freq = (float) 103.83;
+			break;
+		case 45:
+			freq = (float) 110.00;
+			break;
+		case 46:
+			freq = (float) 116.54;
+			break;
+		case 47:
+			freq = (float) 123.47;
+			break;
+		case 48:
+			freq = (float) 130.81;
+			break;
+		case 49:
+			freq = (float) 138.59;
+			break;
+		case 50:
+			freq = (float) 146.83;
+			break;
+		case 51:
+			freq = (float) 155.56;
+			break;
+		case 52:
+			freq = (float) 164.81;
+			break;
+		case 53:
+			freq = (float) 174.61;
+			break;
+		case 54:
+			freq = (float) 185.00;
+			break;
+		case 55:
+			freq = (float) 196.00;
+			break;
+		case 56:
+			freq = (float) 207.65;
+			break;
+		case 57:
+			freq = (float) 220.00;
+			break;
+		case 58:
+			freq = (float) 233.88;
+			break;
+		case 59:
+			freq = (float) 246.94;
+			break;
+		case 60:
+			freq = (float) 261.63;//Middle C
+			break;
+		case 61:
+			freq = (float) 277.18;
+			break;
+		case 62:
+			freq = (float) 293.66;
+			break;
+		case 63:
+			freq = (float) 311.13;
+			break;
+		case 64:
+			freq = (float) 329.63;
+			break;
+		case 65:
+			freq = (float) 349.23;
+			break;
+		case 66:
+			freq = (float) 369.99;
+			break;
+		case 67:
+			freq = (float) 392.00;
+			break;
+		case 68:
+			freq = (float) 415.30;	
+			break;
+		case 69:
+			freq = (float) 440.00;//Tuning A
+			break;
+		case 70:
+			freq = (float) 466.16;
+			break;
+		case 71:
+			freq = (float) 493.88;
+			break;
+		case 72:
+			freq = (float) 523.25;
+			break;
+		case 73:
+			freq = (float) 554.36;
+			break;
+		case 74:
+			freq = (float) 587.32;
+			break;
+		case 75:
+			freq = (float) 622.26;
+			break;
+		case 76:
+			freq = (float) 659.26;
+			break;
+		case 77:
+			freq = (float) 698.46;
+			break;
+		case 78:
+			freq = (float) 739.99;
+			break;
+		case 79:
+			freq = (float) 789.99;
+			break;
+		case 80:
+			freq = (float) 830.61;
+			break;
+		case 81:
+			freq = (float) 880.00;
+			break;
+		case 82:
+			freq = (float) 932.33;
+			break;
+		case 83:
+			freq = (float) 987.77;
+			break;
+		case 84:
+			freq = (float) 1046.50;
+			break;
+		case 85:
+			break;
+		case 86:
+			break;
+		case 87:
+			break;
+		case 88:
+			break;
+		case 89:
+			break;
+		case 90:
+			break;
+		case 91:
+			break;
+		case 92:
+			break;
+		case 93:
+			break;
+		case 94:
+			break;
+		case 95:
+			break;
+		case 96:
+			break;
+		case 97:
+			break;
+		case 98:
+			break;
+		case 99:
+			break;
+		case 100:
+			break;
+		case 101:
+			break;
+		case 102:
+			break;
+		case 103:
+			break;
+		case 104:
+			break;
+		case 105:
+			break;
+		case 106:
+			break;
+		case 107:
+			break;
+		case 108:
+			break;
+		case 109:
+			break;
+		case 110:
+			break;
+		case 111:
+			break;
+		case 112:
+			break;
+		case 113:
+			break;
+		case 114:
+			break;
+		case 115:
+			break;
+		case 116:
+			break;
+		case 117:
+			break;
+		case 118:
+			break;
+		case 119:
+			break;
+		case 120:
+			break;
+		case 121:
+			break;
+		case 122:
+			break;
+		case 123:
+			break;
+		case 124:
+			break;
+		case 125:
+			break;
+		case 126:
+			break;
+		case 127:
+			break;
+		}
+		return freq;
+	}
 	/**
 	 * Create the frame.
 	 */
 	public Interface() {
+		//inicia_lista();
 		UIManager.put("FileChooser.openDialogTitleText", "Abrir arquivo midi");
         UIManager.put("FileChooser.lookInLabelText", "Buscar em");
         UIManager.put("FileChooser.openButtonText", "Abrir");
@@ -153,6 +546,13 @@ public class Interface extends JFrame {
 		JButton btnVisualizar = new JButton("Visualizar");
 		btnVisualizar.setBounds(222, 11, 89, 23);
 		contentPane.add(btnVisualizar);
+		btnVisualizar.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				getTone();
+			}
+		});
 	}
 	public void Preparar()
 	{
@@ -164,6 +564,7 @@ public class Interface extends JFrame {
 			trilhas = s.getTracks();
 			int n_trilhas = trilhas.length;
 			System.out.println("Quantidade de trilhas: " + n_trilhas);
+			//Provavelmente tem de se fazer um merge sort aqui
 			for(int i=0;i<n_trilhas;i++)
 			{
 				System.in.read();
@@ -172,8 +573,9 @@ public class Interface extends JFrame {
 				for(int j=0;j<n_eventos;j++)
 				{
 					MidiEvent e = trilhas[i].get(j);
+					System.out.println("Tempo do evento "+j+": "+e.getTick());
 					MidiMessage msg = e.getMessage();
-					float divtype = s.getDivisionType();
+					//float divtype = s.getDivisionType();
 					if(msg instanceof ShortMessage)
 					{
 							int Command = ((ShortMessage)msg).getCommand();
